67c9f0f08c37216c3662a8b40ecf1d33fce3f634
Hendrik Helwich <hendrik@helwich.de>
2014-04-19 12:45:20 +0200

    skip test in Envjs (takes to long due to setTimeout() impl)

--- src/test/js/introduction.js
+++ src/test/js/introduction.js
@@ -871,23 +871,25 @@ describe("Asynchronous specs", function() {
 
    If the entire suite should have a different timeout, `jasmine.DEFAULT_TIMEOUT_INTERVAL` can be set globally, outside of any given `describe`.
    */
-  describe("long asynchronous specs", function() {
-    var originalTimeout;
-    beforeEach(function() {
-      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    });
+  if (typeof Envjs === "undefined") {
+      describe("long asynchronous specs", function () {
+          var originalTimeout;
+          beforeEach(function () {
+              originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+              jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+          });
 
-    it("takes a long time", function(done) {
-      setTimeout(function() {
-        done();
-      }, 9000);
-    });
+          it("takes a long time", function (done) {
+              setTimeout(function () {
+                  done();
+              }, 9000);
+          });
 
-    afterEach(function() {
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-    });
-  });
+          afterEach(function () {
+              jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+          });
+      });
+  }
 });
 
 
